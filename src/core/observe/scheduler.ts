3bff434ad942a6a400819c97c2fffc60d97c89fe
NinJa427 <17398081021@163.com>
2022-09-28 13:29:56 +0800

    queueWatcher 的实现

--- src/core/observe/scheduler.ts
+++ src/core/observe/scheduler.ts
@@ -1,3 +1,33 @@
 import { Watcher } from './watcher'
 
-export function queueWatcher(watcher: Watcher) {}
+const queue: Watcher[] = []
+let waiting = false
+const has: { [key: number]: boolean } = {}
+let flushing = false
+let index = 0
+
+export function queueWatcher(watcher: Watcher) {
+  const id = watcher.id
+  if (has[id] == null) {
+    has[id] = true
+    if (!flushing) {
+      queue.push(watcher)
+    } else {
+      let i = queue.length - 1
+      while (i > index && queue[i].id > watcher.id) {
+        i--
+      }
+      queue.splice(i + 1, 0, watcher)
+    }
+
+    if (waiting) {
+      waiting = true
+
+      nextTick(flushSchedulerQueue)
+    }
+  }
+}
+
+function flushSchedulerQueue() {}
+
+function nextTick(cb?: Function, ctx?: Object) {}
